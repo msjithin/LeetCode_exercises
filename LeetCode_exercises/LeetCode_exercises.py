1eb6db33c016f49a546331a6e4a3a05412710dd7
Jithin Madhusudanan Sreekala <ms@wisc.edu>
2020-11-25 18:38:28 +0100

    ex 17 added

--- LeetCode_exercises/LeetCode_exercises.py
+++ LeetCode_exercises/LeetCode_exercises.py
@@ -6,4 +6,6 @@ rmDuplicate = ex26.Solution()
 nums = [0,0,1,1,1,2,2,3,3,4]
 print(  rmDuplicate.removeDuplicates(nums) )
 
-import ex0027_removeElement as ex27
+import ex0017_letterCombination as exsn
+ex17 = exsn.Solution
+print( ex17.letterCombinations_recc("234")  )
